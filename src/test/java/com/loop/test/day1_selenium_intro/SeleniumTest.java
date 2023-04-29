d14c7e623f53abb04379c881578b75bff177b5c7
feyruz-loopcamp <feyruz@loopcamp.io>
2023-04-29 15:06:59 -0400

    commit

--- src/test/java/com/loop/test/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/com/loop/test/day1_selenium_intro/SeleniumTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // setting bu web driver
         WebDriverManager.chromedriver().setup();
@@ -14,10 +14,19 @@ public class SeleniumTest {
         WebDriver driver = new ChromeDriver();
 
 
-        driver.get("https://beta.docuport.app/");
+       driver.get("https://www.etsy.com/");
 
+        // navigate to - it allows back and forth
+        driver.navigate().to("https://www.etsy.com/");
+        driver.navigate().back();
+        Thread.sleep(3000);
+        driver.navigate().forward();
+        driver.navigate().refresh();
 
+        // title methods
 
+        String myWebtitle = driver.getTitle();
+        System.out.println("myWebtitle = " + myWebtitle);
 
 
     }
