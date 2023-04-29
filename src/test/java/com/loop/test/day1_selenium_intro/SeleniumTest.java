8049a19da5de593f4548174900b46c01b25a0cfb
feyruz-loopcamp <feyruz@loopcamp.io>
2023-04-29 09:28:04 -0400

    new updates

--- src/test/java/com/loop/test/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/com/loop/test/day1_selenium_intro/SeleniumTest.java
@@ -1,4 +1,4 @@
-package com.loop;
+package com.loop.test.day1_selenium_intro;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
@@ -7,12 +7,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumTest {
     public static void main(String[] args) {
 
-
+        // setting bu web driver
         WebDriverManager.chromedriver().setup();
 
+        // create instance of driver
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.google.com");
+
+        driver.get("https://beta.docuport.app/");
+
+
+
+
 
     }
 }
